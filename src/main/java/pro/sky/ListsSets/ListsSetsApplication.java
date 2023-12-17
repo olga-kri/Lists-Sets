4dd0923de18c3bf9529f8f95f1385594b909a0d4
olga-kri <krivenkoolga1987@gmail.com>
2023-12-18 00:59:29 +0500

    Initial commit

---
+++ src/main/java/pro/sky/ListsSets/ListsSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.ListsSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListsSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListsSetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/ListsSets/ListsSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.ListsSets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListsSetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
